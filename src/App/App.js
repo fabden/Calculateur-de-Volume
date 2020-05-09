af9eb276152c63ee7b77062d429154a9ef1df24d
Fabrice DENNEMONT <fabden@gmail.com>
2020-05-09 13:44:39 +0200

    ajouter/parametrage carte en bootstarp

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './style.css';
+import CartMeuble from './CardFourniture';
 
 function App() {
   return (
-    <div>
-      
-        
-        <p className="h1" >
-         ddvfvfvfvfv
-        </p>
-    
-     
+    <div className='container'>
+      <div className='row'>  
+        <div className='col-8'>  
+        <div className="card-columns">
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      <CartMeuble/>
+      </div>
+      </div> 
+      <div className='col-4 bg-primary'>
+        dffdfdggggggggg
+      </div>
+     </div>
     </div>
-  );
-}
+   ) };
 
 export default App;
---
+++ src/App/CardFourniture/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CartMeuble = ()=>{ 
+
+return(
+    
+    <div className="card">      
+      <div className="card-body">
+        <p className="card-text">Some quiof .</p>
+      </div> 
+      <div className="card-footer row justify-content-around">
+        <button type="button" className="btn btn-danger">+</button>
+        <p className="col-1">         2         </p>
+        <button type="button" className="btn btn-success">-</button>
+      </div>   
+    </div>
+  
+);};
+
+export default CartMeuble;
