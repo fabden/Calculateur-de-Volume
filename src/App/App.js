2648130b91fd4878823741a71c8956c00a438e65
Fabrice DENNEMONT <fabden@gmail.com>
2020-05-09 15:16:55 +0200

    ajouter map sur data test ok

--- src/App/App.js
+++ src/App/App.js
@@ -1,31 +1,25 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './style.css';
 import CartMeuble from './CardFourniture';
+import datasFourniture from '../Datas';
 
 function App() {
+
+const [datas, setDatas] = useState(datasFourniture);
+
+
+
   return (
     <div className='container'>
       <div className='row justify-content-around'>  
           <div className='col-7 border border-secondary rounded'>  
             <div className="card-columns">
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
-          <CartMeuble/>
+              {datas.map((data)=>(<CartMeuble data={data} key={data.id}/>))}        
           </div>
         </div> 
         <div className='col-4 border border-secondary rounded'>
-          <p className='bg-primary'>Estimations Volumes </p>
+          <p className='bg-primary text-center'>Estimations Volumes </p>
+
         </div>
      </div>
     </div>
--- src/App/CardFourniture/index.js
+++ src/App/CardFourniture/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const CartMeuble = ()=>{ 
+const CartMeuble = ({data})=>{ 
 
 return(
     
     <div className="card">      
       <div className="card-body">
-        <p className="card-text"></p>
+        <p className="card-text text-center">{data.Nom}</p>
       </div> 
       <div className="card-footer row justify-content-around">
         <button type="button" className="btn btn-danger">+</button>
-        <p className="col-1">         2         </p>
+          <p className="col-1">{data.Quantité}</p>
         <button type="button" className="btn btn-success">-</button>
       </div>   
     </div>
