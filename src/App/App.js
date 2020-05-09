d3601436bddcbd73b1ec7d1781b36bbb23b2c303
Fabrice DENNEMONT <fabden@gmail.com>
2020-05-09 23:57:31 +0200

    integration supression article cote liste

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,8 @@ import datasFourniture from '../Datas';
 
 function App() {
 
-const [datas, setDatas] = useState(datasFourniture);
+const [datas, setDatas] = useState([...datasFourniture]);
+const [menu, setMenu] = useState("Salon");
 
 // addition soustration 
 
@@ -15,7 +16,7 @@ const addFourniture = (id)=>{
 };
 
 const souFourniture = (id)=>{
-  const newdatas = datas.map((data)=>((data.id === id && data.nombreFou !=0) ? ({...data, nombreFou:data.nombreFou -1 }) : ({...data}) ));
+  const newdatas = datas.map((data)=>((data.id === id && data.nombreFou !==0) ? ({...data, nombreFou:data.nombreFou -1 }) : ({...data}) ));
   setDatas(newdatas);
 };
 
@@ -23,23 +24,74 @@ const souFourniture = (id)=>{
 
 const filterFourniture = datas.filter((d)=> d.nombreFou >0);
 
+//somme des volume 
 
+const total = datas.map((d) => d.Volumes * d.nombreFou).reduce((totale, d) => totale + d, 0);
+
+//fonction selection menu
+const selectMenu = datas.filter((d)=>d.Categorie===menu);
 
 
   return (
-    <div className='container'>
-      <div className='row justify-content-around'>  
-          <div className='col-7 border border-secondary rounded'>  
+    <div className='container h-75 mt-5'>
+      <div className='row justify-content-around '>  
+          <div className='col-lg-7 border border-warning rounded-lg  col-sm-12 col-9'> 
+         <div className="row justify-content-around h1 font-weight-bold bg-warning p-2 rounded-lg">           
+           Categorie
+          </div> 
+          <div className="row justify-content-around mb-2"> 
+            <div className='col'>          
+              <ul className="nav nav-pills nav-justified ">
+                <li className="nav-item ">
+                  <p className="nav-link btn-warning"  onClick={(e)=>{setMenu(e.target.textContent)}}>Salon</p>
+                </li>
+                <li className="nav-item">
+                  <p className="nav-link btn-warning"  onClick={(e)=>{setMenu(e.target.textContent)}}>Cuisine</p>
+                </li>
+                <li className="nav-item">
+                  <p className="nav-link btn-warning"  onClick={(e)=>{setMenu(e.target.textContent)}}>Jardin</p>
+                </li>
+                <li className="nav-item">
+                  <p className="nav-link btn-warning"  onClick={(e)=>{setMenu(e.target.textContent)}}>Chambre</p>
+                </li>
+                <li className="nav-item">
+                  <p className="nav-link btn-warning"  onClick={(e)=>{setMenu(e.target.textContent)}}>Garage</p>
+                </li>
+                <li className="nav-item">
+                  <p className="nav-link btn-warning"  onClick={(e)=>{setMenu(e.target.textContent)}}>Divers</p>
+                </li>
+              </ul>
+              </div>
+          </div> 
+         
+         <div className="row justify-content-center">
+           <div className="col liste">
             <div className="card-columns">
-              {datas.map((data)=>(<CartMeuble data={data} key={data.id} addFourniture={addFourniture} souFourniture={souFourniture}/>))}        
+              {selectMenu.map((data)=>(<CartMeuble data={data} key={data.id} addFourniture={addFourniture} souFourniture={souFourniture}/>))}        
+            </div>
+          </div> 
           </div>
-        </div> 
-        <div className='col-4 border border-secondary rounded'>
-        <p className='bg-primary text-center'>Liste Fourniture</p>
 
-         {filterFourniture.map((d) => <p className='bg-primary'>{d.nombreFou} X {d.Nom}</p> )} 
 
-         <span class="badge badge-warning">Warning</span>     
+        </div>         
+        <div className=' border border-warning rounded-lg col-lg-4 col-sm-12 col-9'>
+        <div className="row justify-content-center  ">
+          <p className='bg-warning text-center col-12 p-3 my-n1 h1 font-weight-bold align-items-start '>Listes </p>
+        </div>    
+        <div className="liste ">
+         {filterFourniture.map((d) => 
+          <div className="row border-bottom border-warning ">
+          <div className="col-9">
+          <p className='  m-2'key= {d.id}>{d.nombreFou} x {d.Nom}</p>
+          </div>
+          <div className="col-1">
+          <button type="button " className="btn btn-outline-dark font-weight-bold py-0 m-2" onClick={()=>souFourniture(d.id)}>-</button>
+          </div>
+          </div> )}               
+         </div>
+         <div className="row justify-content-center align-self-end">
+         <p className="bg-warning text-center col-12 p-3 rounded-bottom h4 my-n1 "> Volumes: <span className="font-weight-bold h4">{total} m²</span></p>
+        </div>
 
         </div>
      </div>
--- src/App/CardFourniture/index.js
+++ src/App/CardFourniture/index.js
@@ -4,17 +4,17 @@ const CartMeuble = ({data,addFourniture,souFourniture})=>{
 
 return(
     
-    <div className="card">      
+    <div className="card border-warning">      
       <div className="card-body">
-        <p className="card-text text-center">{data.Nom}</p>
+        <p className="card-text text-center ">{data.Nom}</p>
       </div> 
-      <div className="card-footer row justify-content-around">
-        <button type="button" className="btn btn-danger" onClick={()=>addFourniture(data.id)}>+</button>
-          <p className="col-4 text-center">{data.nombreFou}</p>
-        <button type="button" className="btn btn-success" onClick={()=>souFourniture(data.id)}>-</button>
-      </div>   
-    </div>
-  
+      
+      <div className="card-footer row">            
+        <button type="button col" className="btn btn-outline-dark font-weight-bold  " onClick={()=>addFourniture(data.id)}>+</button>
+          <p className="col text-center font-weight-bold h4">{data.nombreFou}</p>
+        <button type="button col" className="btn btn-outline-dark font-weight-bold" onClick={()=>souFourniture(data.id)}>-</button>      
+      </div>    
+  </div>   
 );};
 
 export default CartMeuble;
